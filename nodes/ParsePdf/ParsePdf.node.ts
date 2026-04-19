291d71f28b379336aac7b8dac3d3bb8fe451d4d7
Rishabh Dugar <rishabhdugar@Rishabhs-MacBook-Air.local>
2026-04-19 21:40:31 +0530

    fix: lint, icons, publish fixes

--- nodes/ParsePdf/ParsePdf.node.ts
+++ nodes/ParsePdf/ParsePdf.node.ts
@@ -11,7 +11,7 @@ export class ParsePdf implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Parse PDF',
 		name: 'parsePdf',
-		icon: { light: 'file:../../icons/icon.svg', dark: 'file:../../icons/icon.svg' },
+		icon: { light: 'file:../../icons/light.svg', dark: 'file:../../icons/dark.svg' },
 		group: ['transform'],
 		version: 1,
 		description: 'Extract text, tables, and structured data from PDF files using PDF API Hub',
